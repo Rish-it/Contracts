7b8fe4ba038bbf7aee2fef9042e21543ef8ea1ee
Rish-it <201b214@juetguna.in>
2025-02-05 12:37:53 +0530

    none

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,30 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract StkaingContract {
+    mapping (address => uint ) stakes;
+    uint public totalStakes;
+
+    constructor (){
+    }
+
+    function stake(uint   _amount) public payable {
+
+        require(_amount>0);
+        require(_amount == msg.value);
+        stakes[msg.sender] += _amount;
+        totalStakes += _amount;
+
+
+    }
+
+    function unstake(uint  _amount) public {      //sending doesn't need to be payable
+        require(stakes[msg.sender]>= _amount);
+        payable(msg.sender).transfer(_amount);
+        totalStakes -= _amount;
+        stakes[msg.sender] -= _amount;
+
+
+
+    }
+ }
